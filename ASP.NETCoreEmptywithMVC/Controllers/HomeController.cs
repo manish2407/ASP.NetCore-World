fbec564a2e3cdeb473e800266d5bb9f4796e5a25
Manish Sharma <manishms2407@gmail.com>
2026-01-05 23:53:16 +0530

    Add ASP.NETCoreEmptywithMVC project with basic MVC setup and to handle the routing we have used MapDefaultControllerRoute() to hit the controller index method.
    Added new ASP.NET Core MVC project targeting .NET 8.0, including project file, solution registration, and configuration files (appsettings, launchSettings). Implemented Program.cs to configure MVC and routing, and added a basic HomeController with an Index action.

---
+++ ASP.NETCoreEmptywithMVC/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ASP.NETCoreEmptywithMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string Index()
+        {
+            return "This is Index action from MVC Controller";
+        }
+    }
+}
